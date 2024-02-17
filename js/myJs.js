191a0d96dc198c4a51c5573fc7b5b4a88d796752
vufanhiu <160345670+vufanhiu@users.noreply.github.com>
2024-02-18 01:21:51 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "Chào Phong!",
+  text2: "Anh có điều này muốn hỏi bé.",
+  text3: "Bé yêu anh có phải không nào ._.",
+  text4: "Nếu bé ko trả lời mà thoát ra tức là muốn làm ny anh rùi đó nha :v",
+  text5: "Ngáo à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "lí do bé thích anh đi :vvvv",
+  text8: "Gửi cho anh <3",
+  text9: "Vì anh đẹp try vlllll",
+  text10: "Anh biết mà ^^ Yêu bé <33333",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Lên HN anh rước đi date nha :v Còn giờ thì chờ gì nữa mà ko inbox cho anh đi nàooo",
   text12: "Okii lunn <3",
 };
 
